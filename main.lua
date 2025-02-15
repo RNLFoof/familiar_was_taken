68ac4264b0898af5c46eb662ab672645d44976ac
RNLFoof <z.zablotsky@gmail.com>
2025-02-15 15:12:27 -0500

    chore: cleanup

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ local profile_count = 8
 local profiles_per_page = 4
 local selected_profile_filename = "fwt_selected_profile.jkr"
 
+-- TODO: changing the name should update it in the sidebar
 
 function G.FUNCS.deliberately_load_profile_wrapper(delete_prof_data)
     compress_and_save(selected_profile_filename, {G.focused_profile})
@@ -38,75 +39,106 @@ function init()
 end
 init()
 
--- function G.FUNCS.extra_profiles_button()
---     G.UIDEF.profile_select()
--- end
-
--- So I THINK this is gone now? It's the profdile select box with the three (eight) guys on top
--- function G.UIDEF.profile_select()
---   print("yeah can I get uuuhhhhhhh")
---   G.focused_profile = G.focused_profile or G.SETTINGS.profile or 1
-
---   tabs = {}
---   for i=1,profile_count do
---     if love.filesystem.getInfo(i..'/'..'profile.jkr') then G:load_profile(i) end
---     tabs[i] = {
---         label = G.PROFILES[i].name and G.PROFILES[i].name or i,
---         chosen = G.focused_profile == i,
---         tab_definition_function = G.UIDEF.profile_option,
---         tab_definition_function_args = i,
---     }
---   end
-
---   local t =   create_UIBox_generic_options({padding = 0,contents ={
---       {n=G.UIT.R, config={align = "cm", padding = 0, draw_layer = 1, minw = 4}, nodes={
---         create_tabs(
---         {tabs = tabs,
---         snap_to_nav = true}),
---       }},
---   }})
---   return t
--- end
-
-
-function G.UIDEF.fwt_profile_list(from_game_over)
-  print("-------------------------------------------------------3")
+-- Seems to be the whole ass popup?
+function G.UIDEF.fwt_profile_list()
   local profile_pages = {}
   for i = 1, math.ceil(profile_count/profiles_per_page) do
     table.insert(profile_pages, localize('k_page')..' '..tostring(i)..'/'..tostring(math.ceil(profile_count/profiles_per_page)))
   end
-  G.E_MANAGER:add_event(Event({func = (function()
-    G.FUNCS.fwt_change_profile_list_page{cycle_config = {current_option = 1}}
-  return true end)}))
 
-  -- Counts completed challenges, don't care 
-  -- local _ch_comp, _ch_tot = 0, profile_count
-  -- for k, v in ipairs(G.CHALLENGES) do
-  --   if v.id and G.PROFILES[G.SETTINGS.profile].challenge_progress.completed[v.id or ''] then
-  --     _ch_comp = _ch_comp + 1
-  --   end
-  -- end
+  -- Not sure what this does
+  -- Maybe it's what opens the box in the first place?
+  G.E_MANAGER:add_event(
+    Event({
+      func = (function()
+        G.FUNCS.fwt_change_profile_list_page{
+          cycle_config = {
+            current_option = 1   -- TODO or maybe this is how you change the initial page at first?
+          }
+        }
+        return true
+      end)
+    })
+  )
 
-  local t = create_UIBox_generic_options({ back_id = from_game_over and 'from_game_over' or nil, back_func = 'setup_run', back_id = 'fwt_profile_list', contents = {
-    {n=G.UIT.C, config={align = "cm", padding = 0.0, colour=G.C.GREEN}, nodes={
-      {n=G.UIT.R, config={align = "cm", padding = 0.1, minh = 7, minw = 4.2}, nodes={
-        {n=G.UIT.O, config={id = 'fwt_profile_list', object = Moveable()}},
-      }},
-      {n=G.UIT.R, config={align = "cm", padding = 0.1, colour=G.C.RED}, nodes={
-        create_option_cycle({id = 'challenge_page',scale = 0.9, h = 0.3, w = 3.5, options = profile_pages, cycle_shoulders = true, opt_callback = 'fwt_change_profile_list_page',
-        current_option = 1,
-        colour = G.C.RED, no_pips = true, focus_args = {snap_to = true}})
-      }},
-      -- This just lists how many challenges are completed (irrelevant)
-      -- {n=G.UIT.R, config={align = "cm", padding = 0.1, colour=G.C.MONEY}, nodes={
-      --   {n=G.UIT.T, config={text = localize{type = 'variable', key = 'challenges_completed', vars = {_ch_comp, _ch_tot}}, scale = 0.4, colour = G.C.WHITE}},
-      -- }},
-
-    }},
-    {n=G.UIT.C, config={align = "cm", minh = 9, minw = 11.5}, nodes={
-      {n=G.UIT.O, config={id = 'challenge_area', object = Moveable()}},
-    }},
-  }})
+  -- Seems to be the whole ass popup?
+  local t = create_UIBox_generic_options({
+    back_id = 'fwt_profile_list',
+    contents = {
+      {
+        n=G.UIT.C, 
+        config={
+          align = "cm", 
+          padding = 0.0, 
+          colour=G.C.MONEY
+        }, 
+        nodes={
+          {
+            n=G.UIT.R, 
+            config={
+              align = "cm", 
+              padding = 0.1, 
+              minh = 7, 
+              minw = 4.2
+            }, 
+            nodes={
+              {
+                n=G.UIT.O,
+                config={
+                  id = 'fwt_profile_list',
+                  object = Moveable()
+                }
+              },
+            }
+          },
+          {
+            -- The container for the <- Page 1/2 -> guy
+            n=G.UIT.R, 
+            config={
+              align = "cm", 
+              padding = 0.1, 
+              colour=G.C.RED
+            }, 
+            nodes={
+              -- The <- Page 1/2 -> guy
+              create_option_cycle({
+                  id = 'fwt_profile_page',
+                  scale = 0.9, 
+                  h = 0.3, 
+                  w = 3.5, 
+                  options = profile_pages, 
+                  cycle_shoulders = true,
+                  opt_callback = 'fwt_change_profile_list_page',
+                  current_option = 1,  -- TODO hey I bet this is how you change the initial page at first
+                  colour = G.C.RED, 
+                  no_pips = true, 
+                  focus_args = {
+                    snap_to = true
+                  }
+              })
+            }
+          },
+        }
+      },
+      {
+        n=G.UIT.C, 
+        config={
+          align = "cm", 
+          minh = 9, 
+          minw = 11.5
+        }, 
+        nodes={
+          {
+            n=G.UIT.O, 
+            config={
+              id = 'fwt_profile_area', 
+              object = Moveable()
+            }
+          },
+        }
+      },
+    }
+  })
   return t
 end
 
@@ -116,63 +148,103 @@ function roll_focused_profile_with_page(page)
     page = 1
   end
 
-  print("Page is "..page)
-  if G.focused_profile then
-    print("focused_profile was "..G.focused_profile)
-  end
-
   if not G.focused_profile or G.focused_profile == 'nil' or G.focused_profile == nil then
     G.focused_profile = 1 -- TODO this should instead pick the current profile!! but it might not be on this page!! sooooooo
     if G.focused_profile == 'nil' or G.focused_profile == nil then
       print("Hey if you're reading this please ask Dust to fix it so that she can say \"yeah I'll do it later\" and then never do it")
     end
   else
-    print(G.focused_profile)
     -- Adding profiles_per_page because it seems to be able to be negative?
     G.focused_profile = math.fmod(G.focused_profile -1, profiles_per_page) + (page) * profiles_per_page + 1
-    print("focused_profile is "..G.focused_profile)
   end
 end
 
+-- The actual list part, like, the numbered rows, and their container
+-- Said container is in its own container in 
 function G.UIDEF.fwt_profile_list_page(_page)
   -- G.focused_profile = G.focused_profile or G.SETTINGS.profile or 1
   roll_focused_profile_with_page(_page)
   
-  print("-------------------------------------------------------1")
-  local snapped = false
+  -- Snapped is set to false on the first iteration, and true on every other.
+  -- It can be true at first if the below check passes. idk what for
+  local snapped = false 
   local fwt_profile_list = {}
   for k=1,profile_count do
-    if love.filesystem.getInfo(k..'/'..'profile.jkr') then G:load_profile(k) end
-    if not G.PROFILES[k] then
-      G.PROFILES[k] = {}
-    end
-    if not G.PROFILES[k].name then
-      G.PROFILES[k].name = 'P'..k
-    end
-    v = G.PROFILES[k]
+    -- If this profile is actuallyy on the page...
     if k > profiles_per_page*(_page or 0) and k <= profiles_per_page*((_page or 0) + 1) then
-      print("hhhhhhrtjdesjghjredsjgijershjfcsejhfvbhkjdxtvhhjdfhgjikldtf")  
-      if G.CONTROLLER.focused.target and G.CONTROLLER.focused.target.config.id == 'challenge_page' then snapped = true end
 
-        fwt_profile_list[#fwt_profile_list+1] = 
-        {n=G.UIT.R, config={align = "cm"}, nodes={
-          {n=G.UIT.C, config={align = 'cl', minw = 0.8}, nodes = {
-            {n=G.UIT.T, config={text = k..'', scale = 0.4, colour = G.C.WHITE}},
-          }},
-          UIBox_button({id = k, col = true, label = {v.name and G.focused_profile..' '..G.SETTINGS.profile..' '..k..' '..v.name or v.name}, button = 'fwt_change_profile_description', colour = G.C.RED, minw = 4, scale = 0.4, minh = 0.6, focus_args = {snap_to = not snapped}}),
-          {n=G.UIT.C, config={align = 'cm', padding = 0.05, minw = 0.6}, nodes = {
-            {n=G.UIT.C, config={minh = 0.4, minw = 0.4, emboss = 0.05, r = 0.1, colour = G.C.BLUE}, nodes = {
-              -- challenge_completed and {n=G.UIT.O, config={object = Sprite(0,0,0.4,0.4, G.ASSET_ATLAS["icons"], {x=1, y=0})}} or nil
-            }},
-          }},
-        }}      
+      -- Make sure all profiles are initialized (should probably be moved elsewhere?) TODO
+      if love.filesystem.getInfo(k..'/'..'profile.jkr') then G:load_profile(k) end
+      if not G.PROFILES[k] then
+        G.PROFILES[k] = {}
+      end
+      if not G.PROFILES[k].name then
+        G.PROFILES[k].name = 'P'..k
+      end
+    
+      profile_being_rendered = G.PROFILES[k]
+      if G.CONTROLLER.focused.target and G.CONTROLLER.focused.target.config.id == 'fwt_profile_page' then
         snapped = true
+      end
+
+      fwt_profile_list[#fwt_profile_list+1] = 
+      {
+        -- Row in that list, with the number and button
+        n=G.UIT.R, 
+        config={align = "cm"}, 
+        nodes={
+          {
+            n=G.UIT.C, 
+            config={
+              align = 'cl', 
+              minw = 0.8
+            }, 
+            nodes = {
+              -- The numbers to the left of the options
+              {
+                n=G.UIT.T, 
+                config={
+                  text = k..'',
+                  scale = 0.4, 
+                  colour = G.C.WHITE
+                }
+              },
+            }
+          },
+          -- The button that displays the name and you click on to get more information
+          UIBox_button({
+            id = k,
+            col = true, 
+            label = {
+              profile_being_rendered.name and G.focused_profile..' '..G.SETTINGS.profile..' '..k..' '..profile_being_rendered.name
+              or profile_being_rendered.name
+            },
+            button = 'fwt_change_profile_description', -- TODO eyes
+            colour = G.C.RED,
+            minw = 4,
+            scale = 0.4,
+            minh = 0.6,
+            focus_args = {
+              snap_to = not snapped
+            }
+          }),
+
+          -- Held the little radio buttons that show if a challenge is done.
+          -- But they'll be nice if I want extra information later, maybe?
+        -- {n=G.UIT.C, config={align = 'cm', padding = 0.05, minw = 0.6}, nodes = {
+        --   -- {n=G.UIT.C, config={minh = 0.4, minw = 0.4, emboss = 0.05, r = 0.1, colour = G.C.BLUE}, nodes = {
+        --   --   -- challenge_completed and {n=G.UIT.O, config={object = Sprite(0,0,0.4,0.4, G.ASSET_ATLAS["icons"], {x=1, y=0})}} or nil
+        --   -- }},
+        -- }},
+      }}      
+      snapped = true
     end
   end
 
-  return {n=G.UIT.ROOT, config={align = "cm", padding = 0.1, colour = G.C.GREEN}, nodes=fwt_profile_list}
+  return {n=G.UIT.ROOT, config={align = "cm", padding = 0.1, colour = G.C.BLUE}, nodes=fwt_profile_list}
 end
 
+-- Seems to be called when the box is opened and when the page is changed?
 G.FUNCS.fwt_change_profile_list_page = function(args)
   -- Seems the rolling args are:
   -- from_val = from_val,
@@ -180,80 +252,55 @@ G.FUNCS.fwt_change_profile_list_page = function(args)
   --     from_key = from_key,
   --     to_key = to_key,
   --     cycle_config = e.config.ref_table
-  print(args)
   roll_focused_profile_with_page(args.to_key)
-  print("-------------------------------------------------------2")
   if not args or not args.cycle_config then return end
-  print("-------------------------------------------------------22")
   if G.OVERLAY_MENU then
-    print("-------------------------------------------------------222")
     local ch_list = G.OVERLAY_MENU:get_UIE_by_ID('fwt_profile_list')
     if ch_list then 
       if ch_list.config.object then 
         ch_list.config.object:remove() 
       end
-      print("-------------------------------------------------------2222")
       ch_list.config.object = UIBox{
         definition =  G.UIDEF.fwt_profile_list_page(args.cycle_config.current_option-1),
         config = {offset = {x=0,y=0}, align = 'cm', parent = ch_list, colour=G.C.BLACK}
       }
-      print("-------------------------------------------------------22222")
       G.FUNCS.fwt_change_profile_description{config = {id = G.focused_profile, colour=G.C.BLACK}}
-      print("-------------------------------------------------------12")
     end
   end
-  print("-------------------------------------------------------13")
 end
 
 -- Seems e.config.id tells it what to change to?
 G.FUNCS.fwt_change_profile_description = function(e)
-  print("-------------------------------------------------------7")
   if G.OVERLAY_MENU then
-    print("-------------------------------------------------------8")
     
-    local desc_area = G.OVERLAY_MENU:get_UIE_by_ID('challenge_area')
+    -- fwt_profile_list seems to be a 
+    local desc_area = G.OVERLAY_MENU:get_UIE_by_ID('fwt_profile_area')
     if desc_area and desc_area.config.oid ~= e.config.id then
-      print("-------------------------------------------------------9")
-      
-      -- Dunno what this is for and it keeps becoming nil when I don't want it to so I'm trying commenting it out
-      -- Didn't work but idkklkkkkkktrhiht
       if desc_area.config.old_chosen then desc_area.config.old_chosen.config.chosen = nil end
       
       e.config.chosen = 'vert'
-      print("-------------------------------------------------------10")
       if desc_area.config.object then 
         desc_area.config.object:remove() 
       end
-      print("-------------------------------------------------------11")
       desc_area.config.object = UIBox{
         definition =  G.UIDEF.profile_option(e.config.id),
-        config = {offset = {x=0,y=0}, align = 'cm', parent = desc_area}
+        config = {
+          offset = {x=0,y=0}, 
+          align = 'cm', 
+          parent = desc_area}
       }
-      print("-------------------------------------------------------11yyyyyyyyyyyy")
       desc_area.config.oid = e.config.id 
       desc_area.config.old_chosen = e
     end
   end
-  print("-------------------------------------------------------12")
 end
 
 
--- G.FUNCS.deck_view_challenge = function(e)
---   G.FUNCS.overlay_menu{
---     definition = create_UIBox_generic_options({back_func = 'deck_info', contents ={
---         G.UIDEF.challenge_description(get_challenge_int_from_id(e.config.id.id or ''), nil, true)
---       }
---     })
---   }
--- end
-
 function G.FUNCS.profile_select(e)
-  print("-------------------------------------------------------60")
   G.SETTINGS.paused = true
   G.FUNCS.overlay_menu{
     definition = G.UIDEF.fwt_profile_list((false)),
   }
-  print("-------------------------------------------------------61")
 end
 
 function G.FUNCS.original_profile_behavior()
@@ -272,7 +319,6 @@ function G.FUNCS.original_profile_behavior()
     end
     G:load_profile(G.focused_profile)
 
-    print("uwuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu")
     local t = create_UIBox_generic_options({padding = 0,contents ={
         {n=G.UIT.R, config={align = "cm", padding = 0, draw_layer = 1, minw = 4}, nodes={
           create_tabs(
